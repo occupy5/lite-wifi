259c1194efc9b0dfccb5dc24a213c19f3f773054
Fan Shen <1370275510@qq.com>
2019-02-26 16:26:46 +0800

    remove comments

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ var clipboard = new Clipboard(".action-button",{
 var successText = document.querySelector(".action-button");
 clipboard.on("success",function() {
   successText.innerHTML="好了(^〇^)"
-  // console.log("success")
+  console.log("success")
 });
 clipboard.on("error",function() {
   console.log("failed")
-})
+});
